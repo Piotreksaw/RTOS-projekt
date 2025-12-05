cc04de0719e6e37f30bc9be9b55861949b7e88f8
Piotr Sawicki <piotrsawicki13@gmail.com>
2025-12-05 22:53:13 +0100

    inicjalizacja I2C

--- RTOS-L432KC/main.cpp
+++ RTOS-L432KC/main.cpp
@@ -3,9 +3,18 @@
 // Sawicki Piotr 319003
 #include "mbed.h"
 
+// inicjalizacja komunikacji I2C do komunikacji z czujnikiem temperatuy i ciśnienia
+I2C i2c(I2C_SDA, I2C_SCL);
+
+// adres czujnika LPS25HB: 1011101b
+const int LPS25HB_ADDR  = 0x5D << 1;      // adres w hexie
+
+// ostatni bit w adresie odpowiada za odczytywanie lub zapisywanie
+
 
 int main()
 {
+    i2c.frequency(100000);
 
     while (true) {
 
